51aa4ddbfb9a25db62d7f9cccfbfb5194d2f6017
AriyanMlzm <ryanmolazem@gmail.com>
2024-09-15 14:56:17 +0330

    update deployment config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/t-designer-deploy/",
+  base: "/threejs-react-TDesigner/",
   plugins: [react()],
   assetsInclude: ['**/*.glb'],
 })
